f6991751160517418272f5870bb3c060bff72305
Harry Puerta <hyyyp@hotmail.com>
2015-08-26 11:50:51 -0500

    CSS newPC

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,7 +1,7 @@
 module FormHelper
     def setup_pc(pc)
       #pc = Pc.new
-      i = 5 - pc.programs.count
+      i = 10 - pc.programs.count
       i.times {pc.installs.build.build_program}
       pc
     end
